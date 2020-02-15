9d7a19bcbcdb00d6d53dca0b47672f0cd77a077a
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2020-02-15 15:41:39 +0900

    Add PATH setting.

--- setup-rootlessdocker.sh
+++ setup-rootlessdocker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 curl -fsSL https://get.docker.com/rootless | sh
+echo 'export PATH=${HOME}/bin:$PATH' >> ~/.bash_profile
+echo 'export PATH=~$PATH:/sbin' >> ~/.bash_profile
 echo 'export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock' >> ~/.bash_profile
 source ~/.bash_profile
