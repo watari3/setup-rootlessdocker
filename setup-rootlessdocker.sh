bc8603b04108af891b250beaf3986605f0eba3ce
watari3 <koji.ishiwatari@gmail.com>
2020-02-23 18:31:50 +0900

    Update setup-rootlessdocker.sh
    Fix typo.

--- setup-rootlessdocker.sh
+++ setup-rootlessdocker.sh
@@ -2,6 +2,6 @@
 
 curl -fsSL https://get.docker.com/rootless | sh
 echo 'export PATH=${HOME}/bin:$PATH' >> ~/.bash_profile
-echo 'export PATH=~$PATH:/sbin' >> ~/.bash_profile
+echo 'export PATH=$PATH:/sbin' >> ~/.bash_profile
 echo 'export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock' >> ~/.bash_profile
 source ~/.bash_profile
